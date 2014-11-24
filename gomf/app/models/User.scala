46ffd7f805ab10eb0780b707546423d9905a38fd
thiry <thiry@five-seven.net>
2014-11-24 17:12:06 +0900

    ユーザー情報のキャッシュへの格納を実装

--- gomf/app/models/User.scala
+++ gomf/app/models/User.scala
@@ -12,5 +12,17 @@ object User {
    */
   def isLoggedIn : Boolean = Cache.get("User.SteamID") != None
 
-
+  /**
+   * ユーザー情報をキャッシュに登録する
+   * @param userName ユーザー名
+   * @param steamId SteamID
+   * @param profileUrl プロフィールURL
+   * @param avatar アバターURL
+   */
+  def register(userName: String, steamId: String, profileUrl: String, avatar: String) = {
+    Cache.set("User.name", userName)
+    Cache.set("User.SteamID", steamId)
+    Cache.set("User.profileUrl", profileUrl)
+    Cache.set("User.avatar", avatar)
+  }
 }
