180ba6a95fcbc4ee0b4e2915c0561197ff5f2ddc
thiry <thiry@five-seven.net>
2014-11-25 20:23:27 +0900

    ロビーページのレイアウトを実装

---
+++ gomf/public/javascripts/lobby.action.js
@@ -0,0 +1,49 @@
+/**
+ * ロビーページのアクション関連
+ */
+$(function(){
+    var Menu = {
+        //表示するメニューのエレメント
+        element: $('#userMenu'),
+        /**
+         * メニューを表示する
+         * @param target メニューを表示したい位置のエレメント
+         */
+        show: function(target){
+            /**
+             * @var targetの位置
+             */
+            var pos = target.position();
+
+            var self = this;
+
+            //メニューの表示位置を設定し表示する
+            this.element.css({
+                'top': pos.top + (target.height() / 2),
+                'left': pos.left + (target.width() / 2)
+            }).show(1, function() {
+                //メニュー以外をクリックした際にメニューを閉じる
+                $('html').one('click',function ()
+                {
+                    self.element.hide();
+                });
+            });
+
+
+        }
+    };
+
+    /**
+     * フレンドリストクリック時にコールされる
+     */
+    $('ul#friendsList li').on('click', function(){
+        Menu.show($(this));
+    });
+
+    /**
+     * ロビーのプレーヤー一覧クリック時にコールされる
+     */
+    $('#lobby ul#players li').on('click', function(){
+        Menu.show($(this));
+    });
+});
---
+++ gomf/public/javascripts/lobby.layout.js
@@ -0,0 +1,49 @@
+/**
+ * ロビーページのレイアウト関連
+ */
+(function(){
+    /**
+     * レイアウト情報
+     * @type {{}}
+     */
+    var layout = {
+        /**
+         * レイアウトセパレータ
+         */
+        separator: 40,
+        /**
+         * レイアウトの値を計算する
+         */
+        calc: function() {
+            //ブラウザの高さ
+            layout.browserHeight = $(window).outerHeight();
+            //friendボックスの高さ
+            layout.friendsBoxHeight = layout.browserHeight - ($('#friendsList').offset().top + this.separator);
+            //メインコンテンツの高さ
+            layout.mainHeight = layout.browserHeight - ($('#main').offset().top + this.separator);
+
+            return this;
+        },
+        /**
+         * レイアウトを適用する
+         */
+        apply: function() {
+            //フレンドリスト
+            $('#friendsList').outerHeight(layout.friendsBoxHeight);
+            //メイン
+            $('#main').css('min-height', layout.mainHeight);
+        }
+    };
+
+    $(function() {
+        layout.calc().apply();
+    });
+
+    /**
+     * windowリサイズ時にコールされる
+     */
+    $(window).resize(function() {
+        layout.calc().apply();
+    });
+
+})();
