2d1e0bbc161f2a6053a812c1fd7645df97fa83aa
thiry <thiry@five-seven.net>
2014-11-25 22:19:06 +0900

    ロビーページ表示処理を実装

--- gomf/app/controllers/Application.scala
+++ gomf/app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.User
 
 object Application extends Controller {
 
@@ -12,7 +13,24 @@ object Application extends Controller {
   /**
    * ロビーページ
    */
-  def lobby = Action {
-    Ok(utils.Scalate.Template("lobby.jade").render(Map()))
+  def lobby = Action { implicit request =>
+    session.get("steamId") match {
+      //ログインしていない場合トップページへリダイレクト
+      case None => Redirect(routes.Application.index)
+
+      case Some(steamId) => {
+        User.isLoggedIn(steamId) match {
+          //ユーザーデータが見つからない場合はトップページへリダイレクト
+          case false => Redirect (routes.Application.index)
+          case true => {
+            val player = User.info(steamId)
+            val params = Map ('player -> player)
+            Ok(utils.Scalate.Template ("lobby.jade").render(params) )
+          }
+        }
+      }
+
+    }
   }
+
 }
