f0f9e128191f99e7bd302f1370f14532bd3c2c23
thiry <thiry@five-seven.net>
2014-11-27 22:41:31 +0900

    フレンドクリック時のメニューとロビー内プレーヤークリック時のメニューを分離した

--- gomf/public/javascripts/lobby.action.js
+++ gomf/public/javascripts/lobby.action.js
@@ -7,13 +7,40 @@ $(function(){
      * @type {{element: (*|jQuery|HTMLElement), show: show}}
      */
     var Menu = {
-        //表示するメニューのエレメント
-        element: $('#userMenu'),
+        /**
+         * フレンドメニュー
+         */
+        Friend: {
+            //表示するメニューのエレメント
+            element: $('#userMenu'),
+            /**
+             * メニューを表示する
+             * @param target メニューを表示したい位置のエレメント
+             */
+            show: function(target) {
+                Menu.show(target, this.element);
+            }
+        },
+        /**
+         * ロビー内プレーヤーメニュー
+         */
+        Lobby: {
+            //表示するメニューのエレメント
+            element: $('#lobbyMenu'),
+            /**
+             * メニューを表示する
+             * @param target メニューを表示したい位置のエレメント
+             */
+            show: function(target) {
+                Menu.show(target, this.element);
+            }
+        },
         /**
          * メニューを表示する
-         * @param target メニューを表示したい位置のエレメント
+         * @param target 表示する位置
+         * @param menu 表示するメニュー
          */
-        show: function(target) {
+        show: function(target, menu) {
             /**
              * @var targetの位置
              */
@@ -22,13 +49,13 @@ $(function(){
             var self = this;
 
             //メニューの表示位置を設定し表示する
-            this.element.css({
+            menu.css({
                 'top': pos.top + (target.height() / 2),
                 'left': pos.left + (target.width() / 2)
-            }).show(1, function() {
+            }).show(1, function () {
                 //メニュー以外をクリックした際にメニューを閉じる
-                $('html').one('click', function() {
-                    self.element.hide();
+                $('html').one('click', function () {
+                    menu.hide();
                 });
             });
         }
@@ -114,14 +141,14 @@ $(function(){
      * フレンドリストクリック時にコールされる
      */
     $('ul#friendsList li').on('click', function() {
-        Menu.show($(this));
+        Menu.Friend.show($(this));
     });
 
     /**
      * ロビーのプレーヤー一覧クリック時にコールされる
      */
     $('#lobby ul#players li').on('click', function() {
-        Menu.show($(this));
+        Menu.Lobby.show($(this));
     });
 
     /**
