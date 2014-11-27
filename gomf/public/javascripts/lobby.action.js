cd79bc6e4aa210158ff2ea4a34c83e0efc5b5b22
thiry <thiry@five-seven.net>
2014-11-27 22:59:58 +0900

    マップメニューもMAPオブジェクトに統合

--- gomf/public/javascripts/lobby.action.js
+++ gomf/public/javascripts/lobby.action.js
@@ -58,82 +58,80 @@ $(function(){
                     menu.hide();
                 });
             });
-        }
-    };
-
-    /**
-     * MAPメニュー
-     * @type {{}}
-     */
-    var MapMenu = {
-        //表示するメニューのエレメント
-        menuElement: $('#mapSelector'),
-        //選択されているマップの一覧を表示するエレメント
-        shownElement: $('#selectedMapsList'),
-        //選択状態のマップ一覧
-        queueList: [],
+        },
         /**
-         * マップ選択画面を表示する
+         * マップ選択メニュー
          */
-        show: function() {
-            var self = this;
-            //マップ選択画面を表示
-            this.menuElement.show(1, function(){
-                //OKボタンを押した時
-                self.menuElement.find('#mapOK').one('click', function(){
-                    //メニューを閉じる
-                    self.menuElement.hide();
+        Map: {
+            //表示するメニューのエレメント
+            menuElement: $('#mapSelector'),
+            //選択されているマップの一覧を表示するエレメント
+            shownElement: $('#selectedMapsList'),
+            //選択状態のマップ一覧
+            queueList: [],
+            /**
+             * マップ選択画面を表示する
+             */
+            show: function() {
+                var self = this;
+                //マップ選択画面を表示
+                this.menuElement.show(1, function(){
+                    //OKボタンを押した時
+                    self.menuElement.find('#mapOK').one('click', function(){
+                        //メニューを閉じる
+                        self.menuElement.hide();
+                    });
                 });
-            });
-        },
-        /**
-         * マップの選択状態を切り替える
-         */
-        toggleSelect: function(elem) {
+            },
             /**
-             * @var object 選択されたマップのチェックボックスエレメント
+             * マップの選択状態を切り替える
              */
-            var map = elem.find('input.mapCheckBox');
+            toggleSelect: function(elem) {
+                /**
+                 * @var object 選択されたマップのチェックボックスエレメント
+                 */
+                var map = elem.find('input.mapCheckBox');
 
+                /**
+                 * @var String マップ名
+                 */
+                var mapName = map.val();
+
+                //マップ選択状態を切り替える
+                map.prop('checked', !map.prop('checked'));
+
+                /**
+                 * @var boolean マップが選択状態であればtrueが入る
+                 */
+                var isSelected = map.prop('checked');
+
+                //選択されている状態ならqueueリストにマップ名を追加、選択されていなければqueueリストからマップ名を削除する
+                if( isSelected ) {
+                    this.queueList.push(mapName);
+                    //figureにselectedクラスを付与
+                    elem.children('.mapBlock').children('figure').addClass('selected');
+                } else {
+                    this.queueList.remove(mapName);
+                    //figureからselectedクラスを削除
+                    elem.children('.mapBlock').children('figure').removeClass('selected');
+                }
+
+                //選択マップ一覧表示を更新
+                this.shownElement.text(this.queueList.join(" "));
+            },
             /**
-             * @var String マップ名
+             * マップの選択状態をリセットする
              */
-            var mapName = map.val();
+            reset: function() {
+                //キューリストを空にする
+                this.queueList = [];
 
-            //マップ選択状態を切り替える
-            map.prop('checked', !map.prop('checked'));
+                //選択マップ一覧表示を更新
+                this.shownElement.text("");
 
-            /**
-             * @var boolean マップが選択状態であればtrueが入る
-             */
-            var isSelected = map.prop('checked');
-
-            //選択されている状態ならqueueリストにマップ名を追加、選択されていなければqueueリストからマップ名を削除する
-            if( isSelected ) {
-                this.queueList.push(mapName);
-                //figureにselectedクラスを付与
-                elem.children('.mapBlock').children('figure').addClass('selected');
-            } else {
-                this.queueList.remove(mapName);
-                //figureからselectedクラスを削除
-                elem.children('.mapBlock').children('figure').removeClass('selected');
+                //チェックボックスを未チェック状態にする
+                this.menuElement.find('input.mapCheckBox').prop('checked', false);
             }
-
-            //選択マップ一覧表示を更新
-            this.shownElement.text(this.queueList.join(" "));
-        },
-        /**
-         * マップの選択状態をリセットする
-         */
-        reset: function() {
-            //キューリストを空にする
-            this.queueList = [];
-
-            //選択マップ一覧表示を更新
-            this.shownElement.text("");
-
-            //チェックボックスを未チェック状態にする
-            this.menuElement.find('input.mapCheckBox').prop('checked', false);
         }
     };
 
@@ -160,7 +158,7 @@ $(function(){
      * MAP設定ボタンクリック時にコールされる
      */
     $('#mapSettings').on('click', function() {
-        MapMenu.show();
+        Menu.Map.show();
     });
 
     /**
@@ -168,9 +166,9 @@ $(function(){
      */
     $('#mapList li').on('click', function() {
         //マップの選択状態を切り替え
-        MapMenu.toggleSelect($(this));
+        Menu.Map.toggleSelect($(this));
     });
 
     //ブラウザキャッシュによってチェックボックスのチェック状態が保存されている場合があるので初期化
-    MapMenu.reset();
+    Menu.Map.reset();
 });
