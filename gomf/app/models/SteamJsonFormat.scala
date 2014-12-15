482c1b431dd5ebc9c409f4d31756c851ade41c5f
thiry <thiry@five-seven.net>
2014-12-16 00:14:44 +0900

    不要なimportの削除とscaladocの記述をした

--- gomf/app/models/SteamJsonFormat.scala
+++ gomf/app/models/SteamJsonFormat.scala
@@ -1,9 +1,17 @@
 package models
 
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
-
-
+/**
+ * SteamAPIから取得したユーザーデータのフォーマット
+ * @param steamid SteamID
+ * @param communityvisibilitystate コミュニティの表示レベル
+ * @param profilestate プロフィールの表示レベル
+ * @param personname ユーザー名
+ * @param profileurl プロフィールURL
+ * @param avatar アバター画像
+ * @param avatarmedium アバター画像(普通サイズ)
+ * @param avatarfull アバター画像(最大サイズ)
+ * @param personastate ユーザーのステータス
+ */
 case class Player(steamid: String,
                   communityvisibilitystate: Int,
                   profilestate: Int,
@@ -14,6 +22,13 @@ case class Player(steamid: String,
                   avatarfull: String,
                   personastate: Int)
 
+/**
+ * Lobby内で使うユーザーデータのフォーマット
+ * @param steamId SteamID
+ * @param name ユーザー名
+ * @param profileUrl プロフィールURL
+ * @param avatar アバター画像
+ */
 case class PlayerExpression(steamId: String,
                             name: String,
                             profileUrl: String,
