02275c23761d40bd5d3ae6db5e63b178c996d487
thiry <thiry@five-seven.net>
2014-12-30 23:02:39 +0900

    ゲームサーバーの予約処理を追加

--- gomf/app/models/GameServer.scala
+++ gomf/app/models/GameServer.scala
@@ -1,6 +1,6 @@
 package models
+
 import com.github.koraktor.steamcondenser.steam.servers.SourceServer
-import play.api.Play
 import play.api.Play.current
 import play.api.libs.json._
 
@@ -33,12 +33,11 @@ object GameServer {
     }
   }
 
-  def list(): ServerList = {
+  def list: ServerList = {
 
     import scala.collection.JavaConversions._
 
     var servers = immutable.Seq.empty[immutable.Map[String, Any]]
-
     //コンフィグからサーバー情報を取り出す
     play.Play.application.configuration.getConfigList("csgo.servers") foreach { server =>
       val host = server.getString("host")
@@ -56,6 +55,10 @@ object GameServer {
     }
 
     ServerList(servers)
+  }
+
+  def reserve(steamIds: immutable.Seq[String], mapName: String): immutable.Map[String, Any] = {
+    list.reserve(steamIds, mapName)
   }
 }
 
@@ -79,4 +82,132 @@ case class ServerList(servers: immutable.Seq[immutable.Map[String, Any]]) {
     }
     srv
   }
+
+  /**
+   * ゲームサーバー一覧から誰も人がいないサーバーを探し、使用予約する
+   * @return サーバーの接続情報
+   */
+  def reserve(steamIds: immutable.Seq[String], mapName: String) = {
+    this.synchronized{
+      var srv = immutable.Map.empty[String, Any]
+      val breaker = scala.util.control.Breaks
+
+      breaker.breakable {
+        servers foreach { server =>
+          if( server("isEmpty").asInstanceOf[Boolean] ) {
+            val rcon = new Rcon(server("host").toString, server("port").asInstanceOf[Int], server("rconPassword").toString)
+            //サーバーの予約を試みる
+            if( rcon.reserveServer(steamIds) ) {
+              //マップを変更
+              rcon.changelevel(mapName)
+
+              srv = immutable.Map(
+                "host"         -> server("host"),
+                "port"         -> server("port"),
+                "rconPassword" -> server("rconPassword"),
+                "svPassword"   -> server("svPassword")
+              )
+              //サーバーの確保に成功したので以降の処理をスキップ
+              breaker.break()
+            }
+          }
+        }
+      }
+
+      srv
+    }
+  }
+}
+
+/**
+ * サーバーへのRCON通信を行う
+ * @param host ホスト
+ * @param port ポート
+ * @param rconPassword RCONパスワード
+ */
+class Rcon(host: String, port: Int, rconPassword: String) {
+  lazy val server = new SourceServer(host, port)
+  //ゲームサーバーへ接続
+  val rconAuthSuccessful = try {
+    server.rconAuth(rconPassword)
+    true
+  }
+  catch {
+    case _: Throwable => false
+  }
+
+  /**
+   * サーバーが予約状態かチェックする
+   * @return 空いていればtrueを、予約状態ならfalseを返す
+   */
+  def isNotReserved: Boolean = {
+    if( rconAuthSuccessful ) {
+      val reservedStatus = exec("gomf_get_reserve_status")
+      //予約ステータスが空きならば
+      if( reservedStatus == "[GOMF] Server is Free" ) {
+        true
+      } else {
+        //サーバーが予約済みならば
+        false
+      }
+    } else {
+      false
+    }
+  }
+
+  /**
+   * RCONコマンドを実行し、レスポンスを整形する
+   * @param command RCONコマンド
+   * @return レスポンス
+   */
+  def exec(command: String): String = {
+    //コマンド実行
+    val response = server.rconExec(command)
+    //レスポンスから不要な文字列を排除する
+    (response lines ).toSeq(0)
+  }
+
+  /**
+   * サーバーを予約する
+   * @return 予約成功の場合はtrueを、失敗の場合falseを返す
+   */
+  def reserveServer(steamIds: immutable.Seq[String]): Boolean = {
+    if( rconAuthSuccessful ) {
+      if( isNotReserved ) {
+        //サーバーを予約するコマンドを生成
+        val command = "gomf_reserve " + steamIds.mkString(" ")
+        val status = exec(command)
+        if( status == "[GOMF] reserve successful") {
+          //予約成功ならば
+          true
+        } else {
+          false
+        }
+      } else {
+        false
+      }
+    } else {
+      false
+    }
+  }
+
+  def freeServer: Boolean = {
+    if( rconAuthSuccessful ) {
+      val status = exec("gomf_free")
+      if( status == "[GOMF] free successful" ) {
+        true
+      } else {
+        false
+      }
+    } else {
+      false
+    }
+  }
+
+  def changelevel(mapName: String) = {
+    if( rconAuthSuccessful ) {
+      exec("changelevel de_" + mapName)
+    }
+  }
+
 }
